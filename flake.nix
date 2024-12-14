b0e86b247f296c5e19ec0f759c5c613500fe30db
Felipe Kunzler <kunzler.luis@gmail.com>
2024-12-14 20:33:04 -0300

    adiciona manifesto do projeto

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/nixos-24.11";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }@inputs:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs-18_x
+
+            # Comment out one of these to use an alternative package manager.
+            # pkgs.yarn
+            # pkgs.pnpm
+            # pkgs.bun
+
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
