715dae73797156cf232868ebcee2494fdf0af9f6
Felipe Kunzler <kunzler.luis@gmail.com>
2024-12-14 22:05:59 -0300

    adicionando pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>te amo <3</h1>
+}
+
+export default Home;    // ha a possibilidade de varias funcoes serem exportadas
+                        // essa sera a padrao para quando a pagina for renderizada
